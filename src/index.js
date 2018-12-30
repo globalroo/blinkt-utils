2ba123c2d7307b1a00e007287a655aa13b5e4098
Andy Davies <globalroo@hotmail.com>
2018-12-30 03:21:37 +0000

    Add export for new method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { hsv2rgb_a, hsv2rgb_b, hsv2rgb } = require("./hsv2rgb");
+const { hsv2rgb_a, hsv2rgb_b, hsv2rgb_c, hsv2rgb } = require("./hsv2rgb");
 
 module.exports = {
 	hsv2rgb_a: hsv2rgb_a,
